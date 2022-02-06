fb2a4894f07272ff79111e06c4dcc4b5154334c8
Hüseyin Can Çirkin <xyonogcal@gmail.com>
2022-02-06 21:07:22 +0300

    solved 5 problem

--- assignment/assignment.go
+++ assignment/assignment.go
@@ -1,23 +1,60 @@
 package assignment
 
+import (
+	"math"
+	"sort"
+	"strings"
+)
+
 func AddUint32(x, y uint32) (uint32, bool) {
-	return 0, false
+	sumUint64 := uint64(x) + uint64(y)
+	maxUint32 := 1<<32 - 1
+	if sumUint64 > uint64(maxUint32) {
+		return x + y, true
+	}
+	return x + y, false
 }
 
 func CeilNumber(f float64) float64 {
-	return 0
+	return math.Ceil(4*f) / 4
 }
 
 func AlphabetSoup(s string) string {
-	return ""
+	var r []rune
+	for _, runeValue := range s {
+		r = append(r, runeValue)
+	}
+	sort.Slice(r, func(i, j int) bool {
+		return r[i] < r[j]
+	})
+
+	return string(r)
 }
 
 func StringMask(s string, n uint) string {
-	return ""
+	rs := []rune(s)
+	m := int(n)
+	for i := 1; i < len(rs); i++ {
+		if i >= m {
+			rs[i] = '*'
+		}
+	}
+	return string(rs)
 }
 
 func WordSplit(arr [2]string) string {
-	return ""
+	wordsArr := strings.Split(arr[1], ",")
+	var sentence []string
+	for i, _ := range wordsArr {
+		if strings.Index(arr[0], wordsArr[i]) > -1 {
+			sentence = append([]string{wordsArr[i]}, sentence...)
+		}
+	}
+
+	if sentence == nil {
+		return "not possible"
+	}
+	return strings.Join(sentence, ",")
 }
 
 func VariadicSet(i ...interface{}) []interface{} {
